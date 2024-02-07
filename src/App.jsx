d94540caaa000da790da4e085138b350996c9d82
Juliet-07 <ohankwerejulietkelechi@gmail.com>
2024-02-07 16:08:08 +0100

    idea-hub for innovation

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+export default function App() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h2 className="text-red-950 font-semibold uppercase">billion brands</h2>
+      <div className="text-green-500 font-serif underline-offset-0">
+        juliebaby
+      </div>
+      <p>
+        lorem ipsum fkn dfisen fn sdlzsmlj
+        slifesnzfiznnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnndrihf
+        ndsmzkslensrjt4hrgnx dmklfexiorjnxg
+      </p>
+    </div>
+  );
+}
